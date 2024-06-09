012e97b8211c8f18ef6f844ed5cbe0a9e94f7b28
Rolly Maulana Awangga <awangga@gmail.com>
2024-06-09 15:25:15 +0700

    bedakan jenis error

--- controller/face.go
+++ controller/face.go
@@ -14,7 +14,7 @@ func FaceDetect(ctx *fiber.Ctx) error {
 	var h model.Secret
 	err := ctx.ReqHeaderParser(&h)
 	if err != nil {
-		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
+		return ctx.Status(fiber.StatusForbidden).JSON(fiber.Map{"error": err.Error()})
 	}
 	var msg face.FaceDetect
 	err = ctx.BodyParser(&msg)
@@ -23,17 +23,17 @@ func FaceDetect(ctx *fiber.Ctx) error {
 	}
 	buf, err := face.DetectandCropFace(&msg)
 	if err != nil {
-		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
+		return ctx.Status(fiber.StatusConflict).JSON(fiber.Map{"error": err.Error()})
 	}
 	// Create a multipart.FileHeader from the encoded byte slice
 	fileHeader, err := ghupload.CreateFileHeader(buf.GetBytes())
 	if err != nil {
-		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
+		return ctx.Status(fiber.StatusExpectationFailed).JSON(fiber.Map{"error": err.Error()})
 	}
 	// Call GithubUpload with the file header
 	content, response, err := ghupload.GithubUpload(config.GHCreds, fileHeader, "mymyid", "face", msg.IDUser+"/"+msg.IDFile+".jpg", true)
 	if err != nil {
-		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
+		return ctx.Status(fiber.StatusFailedDependency).JSON(fiber.Map{"error": err.Error()})
 	}
 
 	ret := fmt.Sprintf("Upload successful: %v, response: %v\n", content, response)
