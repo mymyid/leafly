4ed8bbf673af010912b252f545a2f538b9d38a27
Rolly Maulana Awangga <awangga@gmail.com>
2024-06-27 06:04:25 +0700

    tambah body limit ke 20 mega upload

--- config/config.go
+++ config/config.go
@@ -16,6 +16,7 @@ var Iteung = fiber.Config{
 	Prefork:       true,
 	CaseSensitive: true,
 	StrictRouting: true,
+	BodyLimit:     20 * 1024 * 1024, // 20MB in bytes
 	ServerHeader:  "DoMyID",
 	AppName:       "Domyikado",
 	Network:       Net,
