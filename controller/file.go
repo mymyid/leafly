e9e7eec7c174ae2e9479b4cc7c28601b2c7e64f4
Rolly Maulana  Awangga <rolly@awang.ga>
2024-07-03 16:16:51 +0700

    repo org dan name jadi input parameter

--- controller/file.go
+++ controller/file.go
@@ -122,7 +122,7 @@ func LogFileMeeting(ctx *fiber.Ctx) error {
 		return ctx.Status(fiber.StatusFailedDependency).JSON(body)
 	}
 	// Call GithubUpload with the file header
-	content, response, _, err := ghupload.GithubUploadFile(config.GHCreds, body.Base64Str, "domyid", "lmsdesa", path, filepath.Ext(body.FileName), false)
+	content, response, _, err := ghupload.GithubUploadFile(config.GHCreds, body.Base64Str, body.RepoOrg, body.RepoName, path, filepath.Ext(body.FileName), false)
 	if err != nil {
 		body.Error = err.Error()
 		return ctx.Status(fiber.StatusFailedDependency).JSON(body)
