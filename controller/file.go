ba946a8fc4f8540ea7ba0603174ef991ff7a1a3c
Rolly Maulana  Awangga <rolly@awang.ga>
2024-07-03 16:11:48 +0700

    tambah model loginfo

--- controller/file.go
+++ controller/file.go
@@ -94,6 +94,47 @@ func ArsipFileLMSDesa(ctx *fiber.Ctx) error {
 	return ctx.Status(fiber.StatusOK).JSON(body)
 }
 
+func LogFileLMSDesa(ctx *fiber.Ctx) error {
+	var h model.Secret
+	var body model.LogInfo
+	err := ctx.ReqHeaderParser(&h)
+	if err != nil {
+		body.Error = err.Error()
+		return ctx.Status(fiber.StatusEarlyHints).JSON(body)
+	}
+	if h.Secret != config.Secret {
+		body.Error = "Secret salah "
+		return ctx.Status(fiber.StatusForbidden).JSON(body)
+	}
+	var msg face.FaceDetect
+	err = ctx.BodyParser(&msg)
+	if err != nil {
+		body.Error = err.Error()
+		return ctx.Status(fiber.StatusBadRequest).JSON(body)
+	}
+	if config.GHCreds.GitHubAccessToken == "" {
+		body.Error = "access token tidak ada: " + config.GHCreds.GitHubAccessToken
+		return ctx.Status(fiber.StatusExpectationFailed).JSON(body)
+	}
+	// Mendapatkan tahun, bulan, dan tanggal sebagai string
+	path, err := GetCurrentDatePathInGMT7()
+	if err != nil {
+		body.Error = err.Error()
+		return ctx.Status(fiber.StatusFailedDependency).JSON(body)
+	}
+	// Call GithubUpload with the file header
+	content, response, _, err := ghupload.GithubUploadFile(config.GHCreds, msg.Base64Str, "domyid", "lmsdesa", path, filepath.Ext(msg.IDFile), false)
+	if err != nil {
+		body.Error = err.Error()
+		return ctx.Status(fiber.StatusFailedDependency).JSON(body)
+	}
+	body.Commit = *content.Commit.SHA
+	body.Remaining = response.Rate.Remaining
+	body.FileHash = *content.Content.Path
+	body.PhoneNumber = msg.IDUser
+	return ctx.Status(fiber.StatusOK).JSON(body)
+}
+
 // Fungsi untuk mendapatkan tanggal dalam zona waktu GMT+7
 func GetCurrentDatePathInGMT7() (path string, err error) {
 	loc, err := time.LoadLocation("Asia/Bangkok")
--- model/model.go
+++ model/model.go
@@ -11,3 +11,16 @@ type FaceInfo struct {
 	FileHash    string `json:"filehash,omitempty" bson:"filehash,omitempty"`
 	Error       string `json:"error,omitempty" bson:"error,omitempty"`
 }
+
+type LogInfo struct {
+	PhoneNumber string `json:"phonenumber,omitempty" bson:"phonenumber,omitempty"`
+	Alias       string `json:"alias,omitempty" bson:"alias,omitempty"`
+	RepoOrg     string `json:"repoorg,omitempty" bson:"repoorg,omitempty"`
+	RepoName    string `json:"reponame,omitempty" bson:"reponame,omitempty"`
+	Commit      string `json:"commit,omitempty" bson:"commit,omitempty"`
+	Remaining   int    `json:"remaining,omitempty" bson:"remaining,omitempty"`
+	FileName    string `json:"filename,omitempty" bson:"filename,omitempty"`
+	Base64Str   string `json:"base64str,omitempty" bson:"base64str,omitempty"`
+	FileHash    string `json:"filehash,omitempty" bson:"filehash,omitempty"`
+	Error       string `json:"error,omitempty" bson:"error,omitempty"`
+}
